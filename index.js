59de6b05a1be120499841edf4d8ed85456cdb177
Yukiyo Torai <yuki@pedros-mbp.home>
2020-03-19 18:26:20 +0000

    Small fix in html

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ function errorCheck() {
     var error_msg = document.getElementById("error-msg");
     error_msg.innerHTML = "Please provide a valid email";
     error_msg.classList.add("error-text");
-    
-    document.getElementById("error-icon").src="images/icon-error.svg";
+
+    document.getElementById("error-icon").src = "images/icon-error.svg";
     document.getElementById("input-form").classList.add("error-form");
     return false;
 };
