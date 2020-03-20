002322a72e1c9727b0cd811c971cde90a379910f
Yukiyo Torai <yuki@pedros-mbp.home>
2020-03-20 16:53:31 +0000

    Fix error icon and modified display

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function errorCheck() {
     error_msg.innerHTML = "Please provide a valid email";
     error_msg.classList.add("error-text");
 
-    document.getElementById("error-icon").src = "images/icon-error.svg";
+    document.getElementById("error-icon").style.display = "block";
     document.getElementById("input-form").classList.add("error-form");
     return false;
 };
