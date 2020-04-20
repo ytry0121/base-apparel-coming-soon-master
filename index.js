1c1489ed542b2c9cbd502cbbceac550ef3b39d60
Yukiyo Torai <yuki@pedros-mbp.home>
2020-04-20 18:49:10 +0100

    modified input validation

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 function errorCheck() {
+  var email_value = document.getElementById("email").value;
+  if(email_value === ""){
     var error_msg = document.getElementById("error-msg");
     error_msg.innerHTML = "Please provide a valid email";
     error_msg.classList.add("error-text");
@@ -6,4 +8,7 @@ function errorCheck() {
     document.getElementById("error-icon").style.display = "block";
     document.getElementById("input-form").classList.add("error-form");
     return false;
+  }
+
+  return true;
 };
